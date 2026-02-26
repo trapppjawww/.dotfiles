e12bbee78a3c4b15568bcc7253828d3d7635e94f
trapppjawww <238725676+trapppjawww@users.noreply.github.com>
2026-02-26 12:48:15 -0800

    emacs: add melpa and multiple-cursors

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,3 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (setq custom-file "~/.config/emacs/custom.el")
 (load custom-file)
 
@@ -7,3 +11,17 @@
 (setq inhibit-startup-screen t)
 (setq make-backup-files nil)
 (setq column-number-mode t)
+
+(require 'multiple-cursors)
+;;; When you have an active region that spans multiple lines, the following
+;;; will add a cursor to each line
+(global-set-key (kbd "C-c C-r") 'mc/edit-lines)
+
+;;; When you want to add multiple cursors not based on continuous lines,
+;;; but based on keywords in the buffer, use:
+(global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-u") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-c C-l") 'mc/skip-to-previous-like-this)
+
